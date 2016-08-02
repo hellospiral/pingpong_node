407ddfeb0f70756047c787410da9cf8b760b57ce
Matthew Carlson <hellospiral@Matthews-MacBook.local>
2016-08-02 15:06:51 -0700

    get weather api functionality working

--- build/js/app.js
+++ build/js/app.js
@@ -1,4 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+exports.apiKey = "8bef59c8248608e4c4db2786f2c7b018";
+
+},{}],2:[function(require,module,exports){
 function Calculator(skinName) {
   this.skin = skinName;
 }
@@ -26,7 +29,7 @@ Calculator.prototype.add = function(num1, num2) {
 
 exports.calculatorModule = Calculator;
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 $(document).ready(function() {
   $('#addition').submit(function(event) {
     event.preventDefault();
@@ -65,17 +68,18 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
-var apiKey = "8bef59c8248608e4c4db2786f2c7b018";
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(response);
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response){
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
     });
   });
 });
 
-},{"./../js/pingpong.js":1}]},{},[2]);
+},{"./../.env":1,"./../js/pingpong.js":2}]},{},[3]);
